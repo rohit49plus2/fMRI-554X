5c01cb8146c80068bc5fddf3f8f350419f3fd05d
Rohit <muralirohit@gmail.com>
2020-11-02 20:10:30 +0530

    testing stuff

---
+++ god_config.py
@@ -0,0 +1,46 @@
+'''Configuration of Generic Object Decoding'''
+
+
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+analysis_name = 'GenericObjectDecoding'
+
+# Data settings
+subjects = {'Subject1' : [dir_path+'/data/Subject1.h5'],
+            'Subject2' : [dir_path+'/data/Subject2.h5'],
+            'Subject3' : [dir_path+'/data/Subject3.h5'],
+            'Subject4' : [dir_path+'/data/Subject4.h5'],
+            'Subject5' : [dir_path+'/data/Subject5.h5']}
+
+rois = {'VC' : 'ROI_VC = 1',
+        'LVC' : 'ROI_LVC = 1',
+        'HVC' : 'ROI_HVC = 1',
+        'V1' : 'ROI_V1 = 1',
+        'V2' : 'ROI_V2 = 1',
+        'V3' : 'ROI_V3 = 1',
+        'V4' : 'ROI_V4 = 1',
+        'LOC' : 'ROI_LOC = 1',
+        'FFA' : 'ROI_FFA = 1',
+        'PPA' : 'ROI_PPA = 1'}
+
+num_voxel = {'VC' : 1000,
+             'LVC' : 1000,
+             'HVC' : 1000,
+             'V1' : 500,
+             'V2' : 500,
+             'V3' : 500,
+             'V4' : 500,
+             'LOC' : 500,
+             'FFA' : 500,
+             'PPA' : 500}
+
+image_feature_file = dir_path+'/data/ImageFeatures.h5'
+features = ['cnn1', 'cnn2', 'cnn3', 'cnn4', 'cnn5', 'cnn6', 'cnn7', 'cnn8', 'hmax1', 'hmax2', 'hmax3', 'gist', 'sift']
+
+# Results settings
+results_dir = dir_path+'/results/'+analysis_name
+results_file = dir_path+'/results/' +analysis_name+'.pkl'
+
+# Figure settings
+roi_labels = ['V1', 'V2', 'V3', 'V4', 'LOC', 'FFA', 'PPA', 'LVC', 'HVC', 'VC']
--- test_img_features.py
+++ test_img_features.py
@@ -1,16 +1,34 @@
 # Test code to see how the data files are stored
 # analysis_FeaturePrediction.py in the /original code folder is a good way to understand
-
-from bdpy import BData
-from bdpy.util import makedir_ifnot, get_refdata
-import numpy as np
+from __future__ import print_function
 import os
-dir_path = os.path.dirname(os.path.realpath(__file__))
-bdata=BData(dir_path+'/original code/data/ImageFeatures.h5')
-# Show 'key' and 'description' of metadata
-bdata.show_metadata()
+import sys
+import pickle
+from itertools import product
+from time import time
+import numpy as np
+import pandas as pd
+from scipy import stats
+from slir import SparseLinearRegression
+from sklearn.linear_model import LinearRegression  # For quick demo
+import bdpy
+from bdpy.bdata import concat_dataset
+from bdpy.ml import add_bias
+from bdpy.preproc import select_top
+from bdpy.stats import corrcoef
+from bdpy.util import makedir_ifnot, get_refdata
+from bdpy.dataform import append_dataframe
+from bdpy.distcomp import DistComp
+from god_config import * #import metadata from this script
+# Main #################################################################
 
-cnn_8=bdata.select('cnn8')
-y_label = bdata.select('ImageID')
-print(cnn_8.shape)
-print(y_label.shape)
+#Load subject 1's data
+subject1=bdpy.BData(subjects['Subject1'])
+image_features=bdpy.BData(image_feature_file)
+for roi in rois:
+    x=subject1.select(rois[roi])#load fMRI data
+    datatype=datatype = dat.select('DataType')   # Data type
+    labels = dat.select('stimulus_id')  # Image labels in brain data
+    print("roi: ", roi)
+    print("datatype: ", datatype)
+    print("labels: ", labels)
