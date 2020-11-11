7ec96689fd044e11a0bd462b3b902194e2d92538
Rohit <muralirohit@gmail.com>
2020-11-12 01:13:11 +0530

    fmri colors

--- rohit-testing.py
+++ rohit-testing.py
@@ -25,30 +25,88 @@ from god_config import * #import metadata from this script
 #Load subject 1's data
 subject1=bdpy.BData(subjects['Subject1'])
 image_features=bdpy.BData(image_feature_file)
-for roi in rois:
+
+
+subject1.show_metadata()
+datatype = subject1.select('DataType')   # Data type
+labels = subject1.select('stimulus_id')  # Image labels in brain data
+voxel_data = subject1.select('VoxelData')  # Image labels in brain data
+voxel_data = subject1.select('VolInds')  # Image labels in brain data
+print("datatype shape: ", datatype.shape)
+print("labels shape: ", labels.shape)
+print("voxel_data shape: ", voxel_data.shape)
+i_train = (datatype == 1).flatten()    # Index for training 1200 trials
+i_test_pt = (datatype == 2).flatten()  # Index for perception test 35 runs of 50 images = 1750
+i_test_im = (datatype == 3).flatten()  # Index for imagery test 20 runs of 25 images
+print("indexes for imagery:", i_test_im)
+print("labels unique",np.unique(labels).shape)
+print("i_train sum",i_train.sum())
+print("i_test_pt sum",i_test_pt.sum())
+print("i_test_im sum",i_test_im.sum())
+
+for roi in {'VC' : 'ROI_VC = 1'}:
     x=subject1.select(rois[roi])#load fMRI data
-    datatype = subject1.select('DataType')   # Data type
-    labels = subject1.select('stimulus_id')  # Image labels in brain data
-    voxel_data = subject1.select('VoxelData')  # Image labels in brain data
-    voxel_data = subject1.select('VolInds')  # Image labels in brain data
     voxel_x = subject1.get_metadata('voxel_x', where=area[roi])
     voxel_y = subject1.get_metadata('voxel_y', where=area[roi])
     voxel_z = subject1.get_metadata('voxel_z', where=area[roi])
     print("roi: ", roi)
     print("x shape: ", x.shape)
-    print("datatype shape: ", datatype.shape)
-    print("labels shape: ", labels.shape)
-    print("voxel_data shape: ", voxel_data.shape)
     print("voxel_x shape: ", voxel_x.shape)
     print("voxel_y shape: ", voxel_y.shape)
     print("voxel_z shape: ", voxel_z.shape)
-    i_train = (datatype == 1).flatten()    # Index for training 1200 trials
-    i_test_pt = (datatype == 2).flatten()  # Index for perception test 35 runs of 50 images = 1750
-    i_test_im = (datatype == 3).flatten()  # Index for imagery test 20 runs of 25 images
-    print("indexes for imagery:", i_test_im)
-    print("labels unique",np.unique(labels).shape)
-    print("i_train sum",i_train.sum())
-    print("i_test_pt sum",i_test_pt.sum())
-    print("i_test_im sum",i_test_im.sum())
-    
-subject1.show_metadata()
+
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+
+def plot_fmri_colors(roi):
+    voxel_x = subject1.get_metadata('voxel_x', where=area[roi])
+    voxel_y = subject1.get_metadata('voxel_y', where=area[roi])
+    voxel_z = subject1.get_metadata('voxel_z', where=area[roi])
+    fig = plt.figure()
+    ax = plt.axes(projection="3d")
+
+    plot=ax.scatter(voxel_x,voxel_y,voxel_z,c=subject1.select(rois[roi])[0][:],cmap='coolwarm')
+    ax.set_xlabel('X Axes')
+    ax.set_ylabel('Y Axes')
+    ax.set_zlabel('Z Axes')
+    ax.set_title(roi)
+
+    fig.colorbar(plot)
+    plt.show()
+
+def coordinates(roi):
+    voxel_x = subject1.get_metadata('voxel_x', where=area[roi])
+    voxel_y = subject1.get_metadata('voxel_y', where=area[roi])
+    voxel_z = subject1.get_metadata('voxel_z', where=area[roi])
+    co=[]
+    for i in range(len(voxel_x)):
+        co.append((voxel_x[i],voxel_y[i],voxel_z[i]))
+
+    return(co)
+
+def padded_coordinates(roi):
+    voxel_x = subject1.get_metadata('voxel_x', where=area[roi])
+    voxel_y = subject1.get_metadata('voxel_y', where=area[roi])
+    voxel_z = subject1.get_metadata('voxel_z', where=area[roi])
+    x_shape=len(voxel_x)
+    y_shape=len(voxel_y)
+    z_shape=len(voxel_z)
+    pc=np.zeros((x_shape,y_shape,z_shape))
+
+
+
+print(len(np.unique(voxel_x)))
+print(len(np.unique(voxel_y)))
+print(len(np.unique(voxel_z)))
+print(len(np.unique(voxel_x))*len(np.unique(voxel_y))*len(np.unique(voxel_z)))
+
+print(coordinates('V4'))
+# plot_fmri_colors('V4')
+# plot_fmri_colors('V3')
+# plot_fmri_colors('V2')
+# plot_fmri_colors('V1')
+# plot_fmri_colors('FFA')
+# plot_fmri_colors('LOC')
+# plot_fmri_colors('PPA')
+plot_fmri_colors('VC')
