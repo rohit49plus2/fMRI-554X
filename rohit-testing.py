ad3e61e34be0ea413886a327d790dd83e8f69ff0
Rohit <muralirohit@gmail.com>
2020-11-12 16:20:07 +0530

    Cleaned up code

--- rohit-testing.py
+++ rohit-testing.py
@@ -26,6 +26,7 @@ from tqdm import tqdm
 
 #Load subject 1's data
 subject1=bdpy.BData(subjects['Subject1'])
+#Load image features
 image_features=bdpy.BData(image_feature_file)
 
 
@@ -34,42 +35,33 @@ datatype = subject1.select('DataType')   # Data type
 labels = subject1.select('stimulus_id')  # Image labels in brain data
 voxel_data = subject1.select('VoxelData')  # Image labels in brain data
 voxel_data = subject1.select('VolInds')  # Image labels in brain data
-print("datatype shape: ", datatype.shape)
-print("labels shape: ", labels.shape)
-print("voxel_data shape: ", voxel_data.shape)
+# print("datatype shape: ", datatype.shape)
+# print("labels shape: ", labels.shape)
+# print("voxel_data shape: ", voxel_data.shape)
 i_train = (datatype == 1).flatten()    # Index for training 1200 trials
 i_test_pt = (datatype == 2).flatten()  # Index for perception test 35 runs of 50 images = 1750
 i_test_im = (datatype == 3).flatten()  # Index for imagery test 20 runs of 25 images
-print("indexes for imagery:", i_test_im)
-print("labels unique",np.unique(labels).shape)
-print("i_train sum",i_train.sum())
-print("i_test_pt sum",i_test_pt.sum())
-print("i_test_im sum",i_test_im.sum())
-
-
-for roi in {'VC' : 'ROI_VC = 1'}:
-    x=subject1.select(rois[roi])#load fMRI data
-    voxel_x = subject1.get_metadata('voxel_x', where=area[roi])
-    voxel_y = subject1.get_metadata('voxel_y', where=area[roi])
-    voxel_z = subject1.get_metadata('voxel_z', where=area[roi])
-    print("roi: ", roi)
-    print("x shape: ", x.shape)
-    print("voxel_x shape: ", voxel_x.shape)
-    print("voxel_y shape: ", voxel_y.shape)
-    print("voxel_z shape: ", voxel_z.shape)
-
+# print("indexes for imagery:", i_test_im)
+# print("labels unique",np.unique(labels).shape)
+# print("i_train sum",i_train.sum())
+# print("i_test_pt sum",i_test_pt.sum())
+# print("i_test_im sum",i_test_im.sum())
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 
-def plot_fmri_colors(roi):
+def plot_fmri_colors(roi,num):
+    """
+    can pass roi as string like 'VC', takes area dictionary from god config
+    plots fmri values in that region for trial number num
+    """
     voxel_x = subject1.get_metadata('voxel_x', where=area[roi])
     voxel_y = subject1.get_metadata('voxel_y', where=area[roi])
     voxel_z = subject1.get_metadata('voxel_z', where=area[roi])
     fig = plt.figure()
     ax = plt.axes(projection="3d")
 
-    plot=ax.scatter(voxel_x,voxel_y,voxel_z,c=subject1.select(rois[roi])[0][:],cmap='coolwarm')
+    plot=ax.scatter(voxel_x,voxel_y,voxel_z,c=subject1.select(rois[roi])[num][:],cmap='coolwarm')
     ax.set_xlabel('X Axes')
     ax.set_ylabel('Y Axes')
     ax.set_zlabel('Z Axes')
@@ -79,6 +71,10 @@ def plot_fmri_colors(roi):
     plt.show()
 
 def coordinates(roi):
+    """
+    can pass roi as string like 'VC', takes area dictionary from god config
+    returns array of size (num_voxels in that roi,) with coordinates.
+    """
     voxel_x = subject1.get_metadata('voxel_x', where=area[roi])
     voxel_y = subject1.get_metadata('voxel_y', where=area[roi])
     voxel_z = subject1.get_metadata('voxel_z', where=area[roi])
@@ -96,7 +92,9 @@ full_y_shape=int((voxel_y.max()-voxel_y.min())/3+1)
 full_z_shape=int((voxel_z.max()-voxel_z.min())/3+1)
 def padded_fmri(roi):
     """
-    takes any region and makes it a 3d array in the shape of a cube that would fit the whole VC
+    pass roi as string like 'VC', takes area dictionary from god config
+    takes fmri from any region and makes it a 3d array in the shape of a cube that would fit the whole VC
+    this is done for all trials.
     """
     voxel_x = subject1.get_metadata('voxel_x', where=area[roi])#might be smaller than full size for other regions
     voxel_y = subject1.get_metadata('voxel_y', where=area[roi])
@@ -122,14 +120,7 @@ def padded_fmri(roi):
 # print(len(np.unique(voxel_x))*len(np.unique(voxel_y))*len(np.unique(voxel_z)))
 
 # print(coordinates('V4'))
-# plot_fmri_colors('V4')
-# plot_fmri_colors('V3')
-# plot_fmri_colors('V2')
-# plot_fmri_colors('V1')
-# plot_fmri_colors('FFA')
-# plot_fmri_colors('LOC')
-# plot_fmri_colors('PPA')
-plot_fmri_colors('VC')
+plot_fmri_colors('VC',5)
 
 vc_padded=padded_fmri('VC')
 print(vc_padded.shape)
