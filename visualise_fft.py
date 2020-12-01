40323b871d16c6335149be6012ae2249a60998cd
rohit <muralirohit@gmail.com>
2020-12-02 00:22:05 +0530

    fft visualisation fixes

--- visualise_fft.py
+++ visualise_fft.py
@@ -28,25 +28,25 @@ for subject in {'Subject1' : dir_path+'/original code/data/Subject1.h5'}: #for n
     x_shape=int((voxel_x.max()-voxel_x.min())/3+1)
     y_shape=int((voxel_y.max()-voxel_y.min())/3+1)
     z_shape=int((voxel_z.max()-voxel_z.min())/3+1)
-    pc=np.zeros((x_shape,y_shape,z_shape))
-    for i in range(len(voxel_x)):
-        x=voxel_x[i]
-        y=voxel_y[i]
-        z=voxel_z[i]
-        x_ind = int((x - voxel_x.min())/3)
-        z_ind = int((z - voxel_z.min())/3)
-        y_ind = int((y - voxel_y.min())/3)
-        pc[x_ind][y_ind][z_ind]=np.abs(fmri[i])
-
-    pc2=np.zeros((x_shape,y_shape,z_shape))
-    for i in range(len(voxel_x)):
-        x=voxel_x[i]
-        y=voxel_y[i]
-        z=voxel_z[i]
-        x_ind = int((x - voxel_x.min())/3)
-        z_ind = int((z - voxel_z.min())/3)
-        y_ind = int((y - voxel_y.min())/3)
-        pc2[x_ind][y_ind][z_ind]=np.abs(fmri2[i])
+    # pc=np.zeros((x_shape,y_shape,z_shape))
+    # for i in range(len(voxel_x)):
+    #     x=voxel_x[i]
+    #     y=voxel_y[i]
+    #     z=voxel_z[i]
+    #     x_ind = int((x - voxel_x.min())/3)
+    #     z_ind = int((z - voxel_z.min())/3)
+    #     y_ind = int((y - voxel_y.min())/3)
+    #     pc[x_ind][y_ind][z_ind]=np.abs(fmri[i])
+    #
+    # pc2=np.zeros((x_shape,y_shape,z_shape))
+    # for i in range(len(voxel_x)):
+    #     x=voxel_x[i]
+    #     y=voxel_y[i]
+    #     z=voxel_z[i]
+    #     x_ind = int((x - voxel_x.min())/3)
+    #     z_ind = int((z - voxel_z.min())/3)
+    #     y_ind = int((y - voxel_y.min())/3)
+    #     pc2[x_ind][y_ind][z_ind]=np.abs(fmri2[i])
 
     # x= np.arange(voxel_x.min(), voxel_x.max()+3, 3)
     # y= np.arange(voxel_y.min(), voxel_y.max()+3, 3)
@@ -62,23 +62,23 @@ for subject in {'Subject1' : dir_path+'/original code/data/Subject1.h5'}: #for n
     print(z.shape)
     xx, yy ,zz = np.meshgrid(x, y, z,indexing='ij')
 
-    fig = plt.figure(figsize=plt.figaspect(0.5))
-    ax = fig.add_subplot(1, 2, 1,projection="3d")
-    plot=ax.scatter(xx.flatten(),yy.flatten(),zz.flatten(),s=20,c=pc,cmap='coolwarm',vmin=-20,vmax=+20)
-    ax.set_xlabel('X Axes')
-    ax.set_ylabel('Y Axes')
-    ax.set_zlabel('Z Axes')
-    ax.set_title("Average Logarithm of Absolute value of FFT fmri \nduring seen experiments in " + roi + " of " + subject,fontsize=16)
-    fig.colorbar(plot,shrink=0.5)
-
-    ax = fig.add_subplot(1, 2, 2, projection='3d')
-    plot=ax.scatter(xx.flatten(),yy.flatten(),zz.flatten(),s=20,c=pc2,cmap='coolwarm',vmin=-20,vmax=+20)
-    ax.set_xlabel('X Axes')
-    ax.set_ylabel('Y Axes')
-    ax.set_zlabel('Z Axes')
-    ax.set_title("Average Logarithm of Absolute value of FFt fmri \nduring imagined experiments in " + roi + " of " + subject,fontsize=16)
-    fig.colorbar(plot,shrink=0.5)
-    plt.show()
+    # fig = plt.figure(figsize=plt.figaspect(0.5))
+    # ax = fig.add_subplot(1, 2, 1,projection="3d")
+    # plot=ax.scatter(xx.flatten(),yy.flatten(),zz.flatten(),s=20,c=pc,cmap='coolwarm',vmin=-20,vmax=+20)
+    # ax.set_xlabel('X Axes')
+    # ax.set_ylabel('Y Axes')
+    # ax.set_zlabel('Z Axes')
+    # ax.set_title("Average Logarithm of Absolute value of FFT fmri \nduring seen experiments in " + roi + " of " + subject,fontsize=16)
+    # fig.colorbar(plot,shrink=0.5)
+    #
+    # ax = fig.add_subplot(1, 2, 2, projection='3d')
+    # plot=ax.scatter(xx.flatten(),yy.flatten(),zz.flatten(),s=20,c=pc2,cmap='coolwarm',vmin=-20,vmax=+20)
+    # ax.set_xlabel('X Axes')
+    # ax.set_ylabel('Y Axes')
+    # ax.set_zlabel('Z Axes')
+    # ax.set_title("Average Logarithm of Absolute value of FFt fmri \nduring imagined experiments in " + roi + " of " + subject,fontsize=16)
+    # fig.colorbar(plot,shrink=0.5)
+    # plt.show()
 
 
 
@@ -102,9 +102,44 @@ for subject in {'Subject1' : dir_path+'/original code/data/Subject1.h5'}: #for n
             z_ind = int((z - voxel_z.min())/3)
             y_ind = int((y - voxel_y.min())/3)
             pc[x_ind][y_ind][z_ind]=np.abs(fmri[i])
-        X.append(np.argmax(pc))
+        X.append(pc)
+    X=np.stack(X)
+    Y=np.array(Y)
     absavgdata = X
-    plt.scatter(Y,absavgdata)
-    plt.xlabel('Seen Vs. Imagined')
-    plt.ylabel('Average For Each Sample')
+    seen = absavgdata[Y==0]
+    imagined = absavgdata[Y==1]
+    pc=np.average(seen,axis=0)
+    pc2=np.average(imagined,axis=0)
+
+    fig = plt.figure(figsize=plt.figaspect(0.5))
+    ax = fig.add_subplot(1, 2, 1,projection="3d")
+    plot=ax.scatter(xx.flatten(),yy.flatten(),zz.flatten(),s=20,c=pc,cmap='coolwarm',vmin=-20,vmax=+20)
+    ax.set_xlabel('X Axes')
+    ax.set_ylabel('Y Axes')
+    ax.set_zlabel('Z Axes')
+    ax.set_title("Average Logarithm of Absolute value of FFT fmri \nduring seen experiments in " + roi + " of " + subject,fontsize=16)
+    fig.colorbar(plot,shrink=0.5)
+
+    ax = fig.add_subplot(1, 2, 2, projection='3d')
+    plot=ax.scatter(xx.flatten(),yy.flatten(),zz.flatten(),s=20,c=pc2,cmap='coolwarm',vmin=-20,vmax=+20)
+    ax.set_xlabel('X Axes')
+    ax.set_ylabel('Y Axes')
+    ax.set_zlabel('Z Axes')
+    ax.set_title("Average Logarithm of Absolute value of FFt fmri \nduring imagined experiments in " + roi + " of " + subject,fontsize=16)
+    fig.colorbar(plot,shrink=0.5)
+    plt.show()
+
+    absavgdata=np.zeros(X.shape[0])
+    for i in range(absavgdata.shape[0]):
+        absavgdata[i]=np.mean(X[i])
+    seen = absavgdata[Y==0]
+    imagined = absavgdata[Y==1]
+
+    bins = np.linspace(0,2.5,1000)
+
+    fig,ax1 = plt.subplots()
+    ax1.hist(seen, bins, alpha = 0.6, color = 'c', label = "Seen")
+    ax2 = ax1.twinx()
+    ax2.hist(imagined, bins, alpha = 0.6, color = 'g', label = "Imagined")
+    fig.legend(bbox_to_anchor = (0.85,0.8))
     plt.show()
